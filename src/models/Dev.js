913da15cb945a68f41b004790e937cc2f2771846
Nubelson <therealnletter@hotmail.com>
2019-08-07 00:11:59 +0100

    Add Dev Model

---
+++ src/models/Dev.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose')
+
+const DevSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true
+		},
+		user: {
+			type: String,
+			required: true
+		},
+		bio: String,
+		avatar: {
+			type: String,
+			required: true
+		}
+	},
+	{
+		timestamps: true
+	}
+)
+
+const Dev = model('Dev', DevSchema)
+module.exports = Dev
