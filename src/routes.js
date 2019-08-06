78139029c593c66e8b47d6204ec461f89893fbe8
Nubelson <therealnletter@hotmail.com>
2019-08-07 00:48:58 +0100

    Controller to signin/up users

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const DevController = require('./controllers/DevController')
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => res.send('Tinder Clone'))
+routes.post('/', DevController.store)
 
 module.exports = routes
