73a39c65274f9e30f1405da0727beb5eda880d27
nubelsondev <nubelsondev@hotmail.com>
2020-01-11 09:59:17 +0000

    Setup database connection

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
-const routes = require('./routes')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
+const routes = require('./routes')
 const port = process.env.PORT || 3333
+const dbUrl = process.env.MONGO_URL
 
 const app = express()
+mongoose.connect(dbUrl, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
 
 app.use(routes)
 
