32d2cdf9738febd5c23c20c12f3d08056daedffd
nubelsondev <nubelsondev@hotmail.com>
2020-01-11 11:32:45 +0000

    like and dislike

---
+++ src/Controllers/DislikeController.js
@@ -0,0 +1,24 @@
+const Dev = require('../Models/Dev')
+
+module.exports = {
+	async store(req, res) {
+		const { user } = req.headers
+		const { devId } = req.params
+
+		const loggedDev = await Dev.findById(user)
+		const targetDev = await Dev.findById(devId)
+
+		if (!targetDev) {
+			return res.status(400).json({ error: 'Dev not exists' })
+		}
+
+		if (loggedDev.dislikes.includes(targetDev._id)) {
+			return res.json({ message: 'Você já deu dislike neste user' })
+		}
+
+		loggedDev.dislikes.push(targetDev._id)
+		await loggedDev.save()
+
+		return res.json(loggedDev)
+	}
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 const routes = require('express').Router()
 const DevController = require('./Controllers/DevController')
+const LikeController = require('./Controllers/LikeController')
+const DislikeController = require('./Controllers/DislikeController')
 
-routes.post('/', DevController.store)
+routes.post('/devs', DevController.store)
+routes.post('/devs/:devId/likes', LikeController.store)
+routes.post('/devs/:devId/dislikes', DislikeController.store)
 
 module.exports = routes
