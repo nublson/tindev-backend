950466068830baace99793593501504527eea8fd
nubelsondev <nubelsondev@hotmail.com>
2020-01-13 00:17:13 +0000

    send data in real time with socket

--- src/server.js
+++ src/server.js
@@ -8,14 +8,31 @@ const port = process.env.PORT || 3333
 const dbUrl = process.env.MONGO_URL
 
 const app = express()
-app.use(cors())
-app.use(express.json())
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+	const { user } = socket.handshake.query
+
+	connectedUsers[user] = socket.id
+})
 
 mongoose.connect(dbUrl, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 })
 
+app.use((req, res, next) => {
+	req.io = io
+	req.connectedUsers = connectedUsers
+
+	return next()
+})
+
+app.use(cors())
+app.use(express.json())
 app.use(routes)
 
-app.listen(port, () => console.log(`Port ${port} is open.`))
+server.listen(port, () => console.log(`Port ${port} is open.`))
